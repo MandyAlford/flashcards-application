5f6b83230ccf08a2635dba7634a044f4622c1a1a
Mandy Alford <mandyalford@gmail.com>
2020-01-30 14:45:57 -0700

    Update Game to pull from data.js

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,26 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Turn = require('../src/Turn');
+const Card = require('../src/Card');
+const Deck = require('../src/Deck');
+const Round = require('../src/Round');
 
 class Game {
   constructor() {}
+
+  start(){
+    var cardArray = prototypeQuestions.map(function(cardObj){
+      return new Card(cardObj);
+    });
+
+    const deck = new Deck(cardArray);
+    const round = new Round(deck);
+
+    this.currentRound = round;
+    this.printMessage(deck, round);
+    this.printQuestion(round);
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
